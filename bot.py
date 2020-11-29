b3f7cb8a2f33f6d2091ed2224677194c3a1e1c19
Mauricio Fernandez <maury844@gmail.com>
2020-11-28 22:09:45 -0400

    Change where the message is output
    Add format to the message

--- bot.py
+++ bot.py
@@ -8,23 +8,25 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_voice_state_update(member, before, after):
     channel = None
     for guild in client.guilds:
-        channel = discord.utils.find(lambda chnl: chnl.name == 'bot', guild.channels)
+        channel = discord.utils.find(
+            lambda chnl: chnl.name == 'bot', guild.channels)
         break
-    
-    await channel.send(f'{before.channel.name} old, {after.channel.name} new')
 
-    # await member.create_dm()
-    # await member.dm_channel.send(
-    #     f'Hi {member.name}, welcome to my Discord server {before}, {after}!'
-    # )
+    name = member.nick
+    source = before.channel.name if before.channel is not None else 'Disconnected'
+    destination = after.channel.name if after.channel is not None else 'Disconnected'
+
+    await channel.send(f'{name} moved from {source} to {destination}')
 
 
 client.run(TOKEN)
